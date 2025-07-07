9e272b54e164414db850a9a6c7149db4677259e4
nishita2094 <nishita.grover@edu.sait.ca>
2025-07-07 14:13:11 -0600

    Update getfacts.tsx

--- components/getfacts.tsx
+++ components/getfacts.tsx
@@ -1,1 +1,21 @@
-// code here
+export const fetchDateFact = async (month: string, day: string) => {
+  if (!month || !day) return null;
+
+  const url = https://numbersapi.p.rapidapi.com/${month}/${day}/date?json=true;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '90128b16a7msh7f1312096cbb0bdp1864fajsn9ff81c9f8be1',
+      'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com',
+    },
+  };
+
+  try {
+    const res = await fetch(url, options);
+    const data = await res.json();
+    return ${data.text} in ${data.year};
+  } catch (err) {
+    return 'Could not fetch fact.';
+  }
+};
